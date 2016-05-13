8700e4927e71197aefd1aa6512204bdd3266db80
Nick Pendery <nick.pendery@greenfieldhq.com>
2016-05-13 16:23:08 -0400

    Add basic betting functionality to game

--- blackjack.ex
+++ blackjack.ex
@@ -1,89 +1,130 @@
 defmodule Blackjack do
-  def game do
-    IO.puts "Hello, welcome to Blackjack. Would you like to start a game?"
+  def sit_at_table do
+    chip_count = 50
+
+    IO.puts "Why hello there, I see that you have #{chip_count} chips. Would you like to play a game of Blackjack?"
     game_start_response = IO.gets "Yes or No "
-
-    if String.rstrip(game_start_response) |> String.capitalize == "Yes" do
-      deck = Deck.create_deck |> Deck.shuffle
-      [player_hand, deck] = Deck.deal_two(deck)
-      [house_hand, deck] = Deck.deal_two(deck)
-
-      Blackjack.dealer_prompt(deck, player_hand, house_hand)
-    else
-      IO.puts "Goodbye"
+    response = String.rstrip(game_start_response) |> String.capitalize
+    case response do
+      "Yes" ->
+        deck = Deck.create_deck |> Deck.shuffle
+        game(deck, chip_count)
+      "Y" ->
+        deck = Deck.create_deck |> Deck.shuffle
+        game(deck, chip_count)
+      "No" ->
+        IO.puts "\nGoodbye"
+      "N" ->
+        IO.puts "\nGoodbye"
+      _ ->
+        IO.puts "\nLets try this again..."
+        Blackjack.sit_at_table
     end
   end
 
-  def dealer_prompt(deck, player_hand, house_hand) do
-    IO.puts "Your hand is:"
+  def game(deck, chip_count) do
+    response = IO.gets "\nWhat would you like to bet on this game? "
+    bet = String.rstrip(response) |> String.to_integer
+    #need to add case for when it is not number
+    case bet do
+      is_integer when bet > chip_count and bet > 0 ->
+        IO.puts "\nYour total chip count is #{chip_count}, please make a bet equal or less to that value"
+        Blackjack.game(deck, chip_count)
+      is_integer when bet > 0 ->
+        [player_hand, deck] = Deck.deal_two(deck)
+        [house_hand, deck] = Deck.deal_two(deck)
+
+        Blackjack.dealer_prompt(deck, player_hand, house_hand, bet, chip_count)
+      _ ->
+        IO.puts "\nPlease enter a number greater than zero"
+        Blackjack.game(deck, chip_count)
+    end
+  end
+
+  def dealer_prompt(deck, player_hand, house_hand, bet, chip_count) do
+    IO.puts "\nYour hand is:"
     for card <- player_hand do
       IO.puts "#{elem(card,0)}#{elem(card,1)}"
     end
     if Hand.sum_value(player_hand, 0) > 21 do
-      IO.puts "Player busts with #{Hand.sum_value(player_hand, 0)}, YOU LOSE!!"
-      Blackjack.play_again
+      IO.puts "\nPlayer busts with #{Hand.sum_value(player_hand, 0)}, YOU LOSE!!"
+      chip_count = chip_count - bet
+      Blackjack.play_again(deck, chip_count)
     else
-      IO.puts "Your hand total is: #{Hand.sum_value(player_hand, 0)}"
-      Blackjack.hit_or_stand(deck, player_hand, house_hand)
+      IO.puts "\nYour hand total is: #{Hand.sum_value(player_hand, 0)}"
+      Blackjack.hit_or_stand(deck, player_hand, house_hand, bet, chip_count)
     end
   end
 
-  def hit_or_stand(deck, player_hand, house_hand) do
-    decision = IO.gets "Would you like to hit or stand? "
+  def hit_or_stand(deck, player_hand, house_hand, bet, chip_count) do
+    decision = IO.gets "\nWould you like to hit or stand? "
     decision = String.rstrip(decision) |> String.capitalize
 
     case decision do
       "Hit" ->
-        Blackjack.player_hit(deck, player_hand, house_hand)
+        Blackjack.player_hit(deck, player_hand, house_hand, bet, chip_count)
       "Stand" ->
-        Blackjack.stand(deck, player_hand, house_hand)
+        Blackjack.stand(deck, player_hand, house_hand, bet, chip_count)
       _ ->
-        IO.puts "Please enter hit or stand"
-        Blackjack.hit_or_stand(deck, player_hand, house_hand)
+        IO.puts "\nPlease enter hit or stand"
+        Blackjack.hit_or_stand(deck, player_hand, house_hand, bet, chip_count)
     end
   end
 
-  def player_hit(deck, player_hand, house_hand) do
+  def player_hit(deck, player_hand, house_hand, bet, chip_count) do
     [new_card, deck] = Deck.deal(deck)
     player_hand = player_hand ++ [new_card]
-    Blackjack.dealer_prompt(deck, player_hand, house_hand)
+    Blackjack.dealer_prompt(deck, player_hand, house_hand, bet, chip_count)
   end
 
-  def house_hit(deck, player_hand, house_hand) do
+  def house_hit(deck, player_hand, house_hand, bet, chip_count) do
     [new_card, deck] = Deck.deal(deck)
     house_hand = house_hand ++ [new_card]
-    Blackjack.stand(deck, player_hand, house_hand)
+    Blackjack.stand(deck, player_hand, house_hand, bet, chip_count)
   end
 
-  def stand(deck, player_hand, house_hand) do
-    IO.puts "Dealer has #{Hand.sum_value(house_hand, 0)}"
+  def stand(deck, player_hand, house_hand, bet, chip_count) do
+    IO.puts "\nThe Dealer's hand is:"
+    for card <- house_hand do
+      IO.puts "#{elem(card,0)}#{elem(card,1)}"
+    end
+    IO.puts "Totalling #{Hand.sum_value(house_hand, 0)}"
+
     house_hand_sum = Hand.sum_value(house_hand, 0)
     player_hand_sum = Hand.sum_value(player_hand, 0)
 
     case house_hand_sum do
       house_hand_sum when house_hand_sum > 21 ->
-        IO.puts "House busts, you win!"
-        Blackjack.play_again
+        IO.puts "\nHouse busts, you win!"
+        chip_count = chip_count + bet
+        Blackjack.play_again(deck, chip_count)
       house_hand_sum when house_hand_sum > player_hand_sum and house_hand_sum > 16 or house_hand_sum == player_hand_sum and house_hand_sum > 16->
-        IO.puts "House Wins!"
-        Blackjack.play_again
+        IO.puts "\nHouse Wins!"
+        chip_count = chip_count - bet
+        Blackjack.play_again(deck, chip_count)
       house_hand_sum when house_hand_sum < player_hand_sum and house_hand_sum > 16 ->
-        IO.puts "Player Wins!"
-        Blackjack.play_again
+        IO.puts "\nPlayer Wins!"
+        chip_count = chip_count + bet
+        Blackjack.play_again(deck, chip_count)
       _ ->
-        IO.gets "Hit enter for the next house card"
-        Blackjack.house_hit(deck, player_hand, house_hand)
+        IO.gets "\nHit enter to see the next house card"
+        Blackjack.house_hit(deck, player_hand, house_hand, bet, chip_count)
     end
   end
 
-  def play_again do
-    IO.puts "Would you like to play again?"
-    game_start_response = IO.gets "Yes or No "
+  def play_again(deck, chip_count) do
+    IO.puts "\nYou now have #{chip_count} chips"
+    if chip_count > 0 do
+      IO.puts "Would you like to play again?"
+      game_start_response = IO.gets "Yes or No "
 
-    if String.rstrip(game_start_response) |> String.capitalize == "Yes" do
-      Blackjack.game
+      if String.rstrip(game_start_response) |> String.capitalize == "Yes" do
+        Blackjack.game(deck, chip_count)
+      else
+        IO.puts "\nThanks for playing, you left the table with #{chip_count} chips!"
+      end
     else
-      IO.puts "Goodbye"
+      "You're out of chips! Come back with more to play again."
     end
   end
 end
